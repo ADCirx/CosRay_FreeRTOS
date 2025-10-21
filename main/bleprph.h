345dba5218594dd6fb7413b78e9ad9e335a3a023
L7_56 <l7_56@foxmail.com>
2025-10-21 18:59:30 +0800

    Update bleprph.h

--- main/bleprph.h
+++ main/bleprph.h
@@ -1,23 +1,8 @@
 #ifndef BLEPRPH_H
 #define BLEPRPH_H
-
-#include <stdbool.h>
-#include "nimble/ble.h"
-#include "modlog/modlog.h"
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-struct ble_hs_cfg;
-struct ble_gatt_register_ctxt;
-
-/** GATT server. */
-#define GATT_SVR_SVC_ALERT_UUID               0x1811
-#define GATT_SVR_CHR_SUP_NEW_ALERT_CAT_UUID   0x2A47
-#define GATT_SVR_CHR_NEW_ALERT                0x2A46
-#define GATT_SVR_CHR_SUP_UNR_ALERT_CAT_UUID   0x2A48
-#define GATT_SVR_CHR_UNR_ALERT_STAT_UUID      0x2A45
-#define GATT_SVR_CHR_ALERT_NOT_CTRL_PT        0x2A44
 
 // 初始化 BLE 模块
 esp_err_t InitBlueTooth(void);
@@ -27,5 +12,4 @@ void AppBlueTooth(void *pvParameters);
 #ifdef __cplusplus
 }
 #endif
-
 #endif // BLEPRPH_H
